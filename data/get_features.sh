bd719cfeac795f58531f8423f9ec773e8f4505e9
penglab <penglab@localhost.localdomain>
2018-08-02 16:39:28 +0800

    pruning added

--- data/get_features.sh
+++ data/get_features.sh
@@ -14,8 +14,8 @@ do
 	# 1. Prepare SWC file
 	awk -v x=${component_no} '{if($2==x)print $0}' $(find ${swc_path}/${cell_name}*swc |awk '{if(NR==1)print $1}') >temp_dir/${cell_type}_${cell_name}.${component_name}.swc
 	awk -F "," -v x=${cell_no} '{if($3==x)print $0}' ${soma_path} >temp_dir/${cell_type}_${cell_name}.${component_name}.apo
-	/home/penglab/Desktop/vaa3d/v3d_external/bin/vaa3d -x sort_neuron_swc_lmg -f sort_swc_lmg -i temp_dir/${cell_type}_${cell_name}.${component_name}.swc -o processed_swc/${cell_type}_${cell_name}.${component_name}.sorted.swc -p 1000
-
+	#/home/penglab/Desktop/vaa3d/v3d_external/bin/vaa3d -x sort_neuron_swc_lmg -f sort_swc_lmg -i temp_dir/${cell_type}_${cell_name}.${component_name}.swc -o processed_swc/${cell_type}_${cell_name}.${component_name}.sorted.swc -p 1000
+	vaa3d -x preprocess -f preprocess -p "#i temp_dir/${cell_type}_${cell_name}.${component_name}.swc #o processed_swc/${cell_type}_${cell_name}.${component_name}.sorted.swc #l 3 #s 0 #t 2 #r 1"
 	# 1.1 Check column names
 	if [ ! -f "feature.names" ]
 		then
@@ -32,6 +32,6 @@ do
 	if [ $(wc -l temp_dir/${cell_type}_${cell_name}.${component_name}.features|awk '{print $1}') -gt 2 ];then paste temp temp_dir/${cell_type}_${cell_name}.${component_name}.features > temp.shadow;fi
 	mv temp.shadow temp;
 done
-rm -r temp_dir
+#rm -r temp_dir
 mv temp ${brain_name}_${cell_type}.${component_name}.features
 
